376f9baee11b37701f1af095aa6a2755d7c5e3c2
iansun <lauer3912@gmail.com>
2016-05-16 13:44:50 +0200

    上传

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"BarcodeUV",
+        appID:"com.romanysoft.app.macos.BarcodeUV",
+        documentTitle:"BarcodeUV By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-76676167-18",
+        gitHome:"//github.com/Romanysoft/BarcodeUV",
+        reportIssueUrl:"//github.com/Romanysoft/BarcodeUV/issues",
+        changeLogUrl:"//github.com/Romanysoft/BarcodeUV/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/BarcodeUV/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
